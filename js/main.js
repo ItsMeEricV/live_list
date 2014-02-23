3f2b765ebeee3acad8988b63888996edb96b0aa7
Eric Vierhaus <ejvaudio@gmail.com>
2014-02-22 23:21:44 -0800

    resetting main

--- js/main.js
+++ js/main.js
@@ -22,61 +22,11 @@ require.config({
     circleplayer: 'libs/jplayer/circle.player',
     grab: 'libs/jplayer/jquery.grab',
     transform2d: 'libs/jplayer/jquery.transform2d',
-    modelbinder: 'libs/modelbinder/Backbone.ModelBinder.min',
-    //smartystreets: '//d79i1fxsrar4t.cloudfront.net/jquery.liveaddress/2.4/jquery.liveaddress',
-    smartystreets: 'libs/smartystreets/liveaddress.min',
-    phoneformat: 'libs/phoneformat/PhoneFormat',
-    queryparams: 'libs/queryparams/backbone.queryparams',
-    videojs: 'libs/videojs/video',
-    jqueryPlaceholder: 'libs/jquery/jquery.placeholder',
-    modernizr: 'libs/modernizr/modernizr.custom.90981',
-    utility: 'libs/utilities/utilities',
-    //minified ace files
-      //ace: 'libs/ace/ace.min',
-      //aceExtra: 'libs/ace/ace-extra.min',
-    //full ace files
-      ace: 'libs/ace/uncompressed/ace',
-      aceExtra: 'libs/ace/uncompressed/ace-extra',
-      aceElement: 'libs/ace/ace-elements.min',
-      jqueryMobile: 'libs/ace/jquery.mobile.custom.min',
-      typeahead: 'libs/ace/typeahead-bs2.min',
-      jqueryUITouchPunch: 'libs/ace/jquery.ui.touch-punch.min',
-      jquerySlimscroll: 'libs/ace/jquery.slimscroll.min',
-      // jqueryEasyPieChart: 'libs/ace/jquery.easy-pie-chart.min',
-      // jquerySparkline: 'libs/ace/jquery.sparkline.min',
-      // jqueryFlot: 'libs/ace/flot/jquery.flot.min',
-      // jqueryFlotPie: 'libs/ace/flot/jquery.flot.pie.min',
-      // jqueryFlotResize: 'libs/ace/flot/jquery.flot.resize.min',
-      jqueryHotkeys: 'libs/ace/jquery.hotkeys.min',
-      jqueryMaskedInput: 'libs/ace/jquery.maskedinput.min',
-      bootstrapWysiwyg: 'libs/ace/bootstrap-wysiwyg.min',
+
     templates: '../templates'
   },
   shim: {
         'bootstrap' : ['jquery'],
-        'jqueryDownload' : ['jquery'],
-        'jqueryScrollTo' : ['jquery'],
-        'jplayer' : ['jquery'],
-        'circleplayer' : ['jquery'],
-        'grab' : ['jquery'],
-        'transform2d' : ['jquery'],
-        'smartystreets' : ['jquery'],
-        'phoneformat' : [],
-        'ace' : ['jquery', 'jqueryUI', 'bootstrap'],
-        'aceElement' : ['jquery'],
-        'aceExtra' : ['jquery'],
-        'jqueryMobile' : ['jquery'],
-        'videojs' : ['jquery'],
-        'jqueryPlaceholder' : ['jquery']
-    },
-    marionette: {
-      deps : ['jquery', 'underscore', 'backbone'],
-      exports : 'Marionette'
-    },
-    ace: {
-        deps: ['jquery', 'aceExtra', 'aceElement', 'bootstrap', 'jqueryMobile', 'jqueryUI', 'typeahead', 'jqueryUITouchPunch','jquerySlimscroll'],
-        exports: 'ace'
-        //'jqueryEasyPieChart','jqueryFlot','jqueryFlotPie','jqueryFlotResize'
     }
 
 });
